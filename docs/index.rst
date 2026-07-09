027dda88cfed43aaa0055cfffbbda0ca2ca0c53f
Joachim Metz <joachim.metz@gmail.com>
2026-07-09 16:31:29 +0200

    Added documentation

---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+Welcome to the CLIToolTester documentation
+=======================================
+
+CLIToolTester, or command line tool tester, provides a framework to run command
+line tools and compare their results.
+
+The source code is available from the `project page <https://github.com/dfirlabs/clitooltester>`__.
+
+.. toctree::
+   :maxdepth: 2
+
+   API documentation <sources/api/clitooltester>
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+
--- utils/update_release.sh
+++ utils/update_release.sh
@@ -3,30 +3,10 @@
 # Script that makes changes in preparation of a new release, such as updating
 # the version and documentation.
 
-EXIT_FAILURE=1;
-EXIT_SUCCESS=0;
-
 VERSION=`date -u +"%Y%m%d"`
-DPKG_DATE=`date -R`
 
 # Update the Python module version.
-sed "s/__version__ = '[0-9]*'/__version__ = '${VERSION}'/" -i dfdatetime/__init__.py
-
-# Update the version in the dpkg configuration files.
-cat > config/dpkg/changelog << EOT
-dfdatetime (${VERSION}-1) unstable; urgency=low
-
-  * Auto-generated
-
- -- Log2Timeline maintainers <log2timeline-maintainers@googlegroups.com>  ${DPKG_DATE}
-EOT
-
-# Regenerate the supported formats documentation.
-# TODO: generate supported formats.
-# PYTHONPATH=. ./utils/export_supported_formats.py > docs/sources/Supported-formats.md
+sed "s/__version__ = '[0-9]*'/__version__ = '${VERSION}'/" -i clitooltester/__init__.py
 
 # Regenerate the API documentation.
 tox -edocs
-
-exit ${EXIT_SUCCESS};
-
