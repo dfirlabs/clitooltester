a755125594e005cb49ab53d5fd64b2fce5ebd385
Joachim Metz <joachim.metz@gmail.com>
2026-07-09 06:20:27 +0200

    Worked on framework and runner

--- clitooltester/yaml_definitions_file.py
+++ clitooltester/yaml_definitions_file.py
@@ -5,11 +5,88 @@ import yaml
 from clitooltester import resources
 
 
-class YAMLTestsDefinitionsFile:
-    """YAML-based tests definitions file.
+class YAMLInputsDefinitionsFile:
+    """YAML-based inputs definitions file.
 
-    A YAML-based tests definitions file contains one or more test definitions. A test
-    definition consists of:
+    A YAML-based inputs definitions file contains one or more input definitions. An
+    input definition consists of:
+
+    name: ext2
+    path: test_data/ext2.raw
+
+    Where:
+    * name, that uniquely identifies the input;
+    * path, location of the input.
+    """
+
+    _SUPPORTED_KEYS = frozenset(["name", "path"])
+
+    def _ReadInputDefinition(self, yaml_input_definition):
+        """Reads an input definition from a dictionary.
+
+        Args:
+          yaml_input_definition (dict[str, object]): YAML input definition values.
+
+        Returns:
+          InputDefinition: input definition.
+
+        Raises:
+          RuntimeError: if the format of the formatter definition is not set
+              or incorrect.
+        """
+        if not yaml_input_definition:
+            raise RuntimeError("Missing input definition values.")
+
+        different_keys = set(yaml_input_definition) - self._SUPPORTED_KEYS
+        if different_keys:
+            different_keys = ", ".join(different_keys)
+            raise RuntimeError(f"Undefined keys: {different_keys:s}")
+
+        name = yaml_input_definition.get("name")
+        if not name:
+            raise RuntimeError("Invalid input definition missing name.")
+
+        path = yaml_input_definition.get("path")
+        if not path:
+            raise RuntimeError("Invalid input definition missing path.")
+
+        input_definition = resources.InputDefinition()
+        input_definition.name = name
+        input_definition.path = path
+
+        return input_definition
+
+    def _ReadFromFileObject(self, file_object):
+        """Reads the event formatters from a file-like object.
+
+        Args:
+          file_object (file): formatters file-like object.
+
+        Yields:
+          InputDefinition: input definition.
+        """
+        yaml_generator = yaml.safe_load_all(file_object)
+
+        for yaml_input_definition in yaml_generator:
+            yield self._ReadInputDefinition(yaml_input_definition)
+
+    def ReadFromFile(self, path):
+        """Reads the event formatters from a YAML file.
+
+        Args:
+          path (str): path to a formatters file.
+
+        Yields:
+          InputDefinition: input definition.
+        """
+        with open(path, encoding="utf-8") as file_object:
+            yield from self._ReadFromFileObject(file_object)
+
+
+class YAMLTestDefinitionFile:
+    """YAML-based test definitions file.
+
+    A YAML-based test definitions file contains a test definition, which consists of:
 
     name: plaso_log2timeline_with_image.qcow2
     command: log2timeline.py unattended %input%
